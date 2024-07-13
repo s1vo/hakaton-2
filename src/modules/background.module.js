28ab6f0828658d0e3fa46ac6ac5ca0697555be1d
InnaAlexy <life.inna.am@yandex.ru>
2024-07-13 13:13:06 +0300

    add background-module

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -6,8 +6,23 @@ export class BackgroundModule extends Module {
         console.log('BackgroundModule created');
     }
 
+    getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }; 
+
     trigger() {
-        console.log('BackgroundModule triggered');
+        let backColor = this.getRandomColor();
+        const body = document.querySelector('body');
+        body.style.backgroundColor = `${backColor}`;
     }
+
+
+
+    
 
 }
